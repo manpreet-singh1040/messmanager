7388fd2e9fb32f423ab56652dcfed5115e61a783
manpreet-singh1040 <147172312+manpreet-singh1040@users.noreply.github.com>
2024-03-04 01:13:29 +0530

    comit

--- src/components/login.js
+++ src/components/login.js
@@ -1,10 +1,6 @@
 import { Component } from "react";
 import './login.css';
 class Login extends Component{
-    constructor(props){
-        super(props);
-
-    }
     render(){
         return(
             <div>
