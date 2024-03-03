97a69418148d84c44c4c24ea44c6d5c60ca896df
manpreet-singh1040 <147172312+manpreet-singh1040@users.noreply.github.com>
2024-03-04 00:49:43 +0530

    remove usestate

--- src/components/rollnoform.js
+++ src/components/rollnoform.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import DisplayBill from "./homePage/displayBill";
 import "./rollnoform.css";
 const Rollnoform=()=>{
